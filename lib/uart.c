da0ddb399cc0a23cabbb70086431eea9f54e05be
hoangthien <a11718thien@gmail.com>
2024-04-30 21:41:41 +0700

    first

---
+++ lib/uart.c
@@ -0,0 +1,76 @@
+/**
+ * @file       <file_name>.c
+ * @copyright  Copyright (C) 2019 Fiot Co., Ltd. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    major.minor.patch
+ * @date       yyyy-mm-dd
+ * @author     <first_name_1> <last_name_1>
+ * @author     <first_name_2> <last_name_2>
+ *             
+ * @brief      <A brief description of the content of the file>
+ *             
+ * @note          
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Includes ----------------------------------------------------------- */
+#include "uart.h"
+
+/* Private defines ---------------------------------------------------- */
+/*!< Description of PRIVATE_DEFINE_1 */
+/*!< Description of PRIVATE_DEFINE_2 */
+
+/* Private enumerate/structure ---------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+
+/**
+ * @brief <structure descriptiton>
+ */
+
+
+/* Private macros ----------------------------------------------------- */
+/**
+ * @brief  <macro description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+/* Public variables --------------------------------------------------- */
+
+
+/* Private variables -------------------------------------------------- */
+/**< Description of private variable var_1 */
+ /**< Description of private variable var_2 */
+
+/* Private function prototypes ---------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return  
+ *  - 0: Success
+ *  - 1: Error
+ */
+
+/* Function definitions ----------------------------------------------- */
+
+
+/* End of file -------------------------------------------------------- */
---
+++ lib/uart.h
@@ -0,0 +1,77 @@
+/**
+ * @file       <file_name>.h
+ * @copyright  Copyright (C) 2019 Fiot Co., Ltd. All rights reserved.
+ * @license    This project is released under the Fiot License.
+ * @version    major.minor.patch
+ * @date       yyyy-mm-dd
+ * @author     <first_name_1> <last_name_1>
+ * @author     <first_name_2> <last_name_2>
+ *
+ * @brief      <A brief description of the content of the file>
+ *
+ * @note
+ * @example    example_file_1.c
+ *             Example_1 description
+ * @example    example_file_2.c
+ *             Example_2 description
+ */
+
+/* Define to prevent recursive inclusion ------------------------------ */
+#ifndef UART_H_
+#define UART_H_
+
+/* Includes ----------------------------------------------------------- */
+#include "stm32f1xx_hal.h"
+/* Public defines ----------------------------------------------------- */
+#define PUBLIC_DEFINE_1  (0) /*!< Description of PUBLIC_DEFINE_1 */
+#define PUBLIC_DEFINE_2  (0) /*!< Description of PUBLIC_DEFINE_2 */
+
+/* Public enumerate/structure ----------------------------------------- */
+/**
+ * @brief <enum descriptiton>
+ */
+
+
+/**
+ * @brief <structure descriptiton>
+ */
+
+/* Public macros ------------------------------------------------------ */
+/**
+ * @brief  <macro description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+#define PUBLIC_MACRO(a)  do_something_with(a)
+
+/* Public variables --------------------------------------------------- */
+extern int g_var_1; /**< Description of public variable g_var_1 */
+extern int g_var_2; /**< Description of public variable g_var_2 */
+
+/* Public function prototypes ----------------------------------------- */
+/**
+ * @brief  <function description>
+ *
+ * @param[in]     <param_name>  <param_despcription>
+ * @param[out]    <param_name>  <param_despcription>
+ * @param[inout]  <param_name>  <param_despcription>
+ *
+ * @attention  <API attention note>
+ *
+ * @return
+ *  - 0: Success
+ *  - 1: Error
+ */
+void public_function(void);
+
+#endif // __CODE_TEMPLATE_H
+
+/* End of file -------------------------------------------------------- */
